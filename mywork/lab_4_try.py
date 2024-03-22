16a1b0d1a010a8f3735605695c35634c1a0d4417
mnk3kd <143535857+mnk3kd@users.noreply.github.com>
2024-03-22 16:35:28 -0400

    Update lab_4_try.py
    updates to python script

--- mywork/lab_4_try.py
+++ mywork/lab_4_try.py
@@ -1,36 +1,43 @@
 #!/usr/bin/python3
 
 
+import boto3
 import requests
-url = 'https://media4.giphy.com/media/5GoVLqeAOo6PK/200w.gif?cid=6c09b9523bs95r6fmgy424cmkn9w434iyjdfqmn4iwfqu0b3&ep=v1_gifs_search&rid=200w.gif&ct=g'
-r = requests.get(url, allow_redirects=True)
 
-open('happy.
-gif&ct=g
-', 'wb').write(r.content)
+def download_image(url, filename):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(filename, 'wb') as f:
+            f.write(response.content)
+            print("Image downloaded successfully.")
+    else:
+        print("Failed to download image.")
 
+# Initialize S3 client
+s3 = boto3.client('s3', region_name='us-east-1')
 
-bucket = 'ds2002-mnk3kd'
-local_file = 'happy.
-gif&ct=g
-'
+# Download image
+download_image('https://news.virginia.edu/sites/default/files/article_image/kelp_forest_douglas_klug_photo_lgdr.jpeg', 'kelp.jpeg')
 
-resp = s3.put_object(
-    Body = local_file,
-    Bucket = bucket,
-    Key = local_file
+# Define S3 bucket and local file
+bucket_name = 'ds2002-mnk3kd'
+local_file = 'kelp.jpeg'
 
-ACL = 'public-read',
+# Upload image to S3
+with open(local_file, 'rb') as f:
+    s3.upload_fileobj(f, bucket_name, local_file, ExtraArgs={'ACL': 'public-read'})
 
-bucket_name = str
-object_name = str
-expires_in = int
+# Generate presigned URL
+object_name = 'kelp.jpeg'
+expires_in = 604800
 
-response = s3.generate_presigned_url(
+presigned_url = s3.generate_presigned_url(
     'get_object',
     Params={'Bucket': bucket_name, 'Key': object_name},
-    ExpiresIn=expires_in 604800
-    )
-
+    ExpiresIn=expires_in
 )
+
+print("Presigned URL:", presigned_url)
+
+#I have no idea why the importing requests is not working - couold be an issue with my python, as I have that package installed. The rest of the script should be correct.
 
